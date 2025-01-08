c4f42b6fb30a747ae4ec9805ae6b8b440a943705
St4YZ <st4yzwd@gmail.com>
2025-01-08 15:37:25 +0200

    Code refactoring

--- tests/e-commerce/e-commerce.spec.ts
+++ tests/e-commerce/e-commerce.spec.ts
@@ -2,29 +2,34 @@ import { test, expect } from '@playwright/test';
 import { MainPage } from '../../PageObject/mainPage';
 import { ActionElements } from '../../PageObject/e-commerce/commerceElements';
 
-test('Magento e-commerce first task', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page);
-    await mainPage.gotoMagentoMainPage()
-    await actionElements.selectProductAndAddItToCart()
-    await expect(actionElements.successAlertMessage).toBeVisible()
-    await actionElements.clickAtMyCardandProceedToCheckoutButtons()
-    await actionElements.fillAllFieldsToBuyProduct()
-    await actionElements.acceptOrder()
-    await expect(actionElements.successOrderMessage).toHaveText('Thank you for your purchase!')
+test.describe('Magento e-commerce', () => {
+    let mainPage: MainPage;
+    let actionElements: ActionElements;
+    test.beforeEach(async ({ page }) => {
+        mainPage = new MainPage(page);
+        actionElements = new ActionElements(page)
+    })
+    test('Magento e-commerce first task', async () => {
+        await mainPage.gotoMagentoMainPage()
+        await actionElements.selectProductAndAddItToCart()
+        await expect(actionElements.successAlertMessage).toBeVisible()
+        await actionElements.clickAtMyCardandProceedToCheckoutButtons()
+        await actionElements.fillAllFieldsToBuyProduct()
+        await actionElements.acceptOrder()
+        await expect(actionElements.successOrderMessage).toHaveText('Thank you for your purchase!')
+    });
+
+    test('Magento e-commerce second task', async () => {
+        await mainPage.gotoMagentoMainPage()
+        await actionElements.selectFirstProductAndAddItToComparison()
+        await actionElements.selectSecondProductAndGotoComparisonPage()
+        await actionElements.clickAtLowestPriceProductItem()
+        await actionElements.addItemToCart()
+        await expect(actionElements.successAlertMessage).toBeVisible()
+        await actionElements.clickAtMyCartLinkAndProceedToCheckoutButton()
+        await actionElements.fillAllFieldsToBuyProduct()
+        await actionElements.acceptOrder()
+        await expect(actionElements.successOrderMessage).toHaveText('Thank you for your purchase!')
+    });
 });
 
-test('Magento e-commerce second task', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page);
-    await mainPage.gotoMagentoMainPage()
-    await actionElements.selectFirstProductAndAddItToComparison()
-    await actionElements.selectSecondProductAndGotoComparisonPage()
-    await actionElements.clickAtLowestPriceProductItem()
-    await actionElements.addItemToCart()
-    await expect(actionElements.successAlertMessage).toBeVisible()
-    await actionElements.clickAtMyCartLinkAndProceedToCheckoutButton()
-    await actionElements.fillAllFieldsToBuyProduct()
-    await actionElements.acceptOrder()
-    await expect(actionElements.successOrderMessage).toHaveText('Thank you for your purchase!')
-});
--- tests/practice/locators-actions.spec.ts
+++ tests/practice/locators-actions.spec.ts
@@ -2,60 +2,54 @@ import { test, expect } from '@playwright/test';
 import { MainPage } from '../../PageObject/mainPage';
 import { ActionElements } from '../../PageObject/webElements';
 
-test('Register page', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoRegisterPage()
-    await actionElements.fillFirstNameField()
-    await actionElements.fillLastNameField()
-    await actionElements.fillPhoneNumberField()
-    await actionElements.selectCountryDropdownOption()
-    await actionElements.fillEmailAddressField()
-    await actionElements.fillPasswordField()
-    await actionElements.selectCheckboxOption()
-    await actionElements.clickRegisterButton()
+test.describe('Locators and Actions', () => {
+    let mainPage: MainPage;
+    let actionElements: ActionElements;
+    test.beforeEach(async ({ page }) => {
+        mainPage = new MainPage(page);
+        actionElements = new ActionElements(page)
+    })
 
-    await expect(actionElements.alertDialog).toBeVisible()
-});
+    test('Register page', async () => {
+        await mainPage.gotoRegisterPage()
+        await actionElements.fillFirstNameField()
+        await actionElements.fillLastNameField()
+        await actionElements.fillPhoneNumberField()
+        await actionElements.selectCountryDropdownOption()
+        await actionElements.fillEmailAddressField()
+        await actionElements.fillPasswordField()
+        await actionElements.selectCheckboxOption()
+        await actionElements.clickRegisterButton()
 
-test('IFrame page', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoIFramePage()
+        await expect(actionElements.alertDialog).toBeVisible()
+    });
 
-    await actionElements.clickAtIFrameLearnMoreButton()
-    await expect(actionElements.iFrameAlertLocator).toBeVisible()
-});
+    test('IFrame page', async () => {
+        await mainPage.gotoIFramePage()
+        await actionElements.clickAtIFrameLearnMoreButton()
+        await expect(actionElements.iFrameAlertLocator).toBeVisible()
+    });
 
-test('Checkbox page', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoCheckBoxPage()
+    test('Checkbox page', async () => {
+        await mainPage.gotoCheckBoxPage()
+        await actionElements.checkAllCheckBoxes()
+        await actionElements.allCheckBoxesAreChecked()
+    });
 
-    await actionElements.checkAllCheckBoxes()
-    await actionElements.allCheckBoxesAreChecked()
-});
+    test('Letcode assert table sum price is correct Task', async () => {
+        await mainPage.gotoLetCodeTables()
+        await actionElements.assertTableData()
+    });
 
-test('Letcode assert table sum price is correct Task', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoLetCodeTables()
+    test('Letcode assert Raj is checked Task', async () => {
+        await mainPage.gotoLetCodeTables()
+        await actionElements.checkRajCheckbox()
+        await expect(actionElements.rajCheckbox).toBeChecked()
+    });
 
-    await actionElements.assertTableData()
-});
+    test.skip('Letcode assert sorting is working correct Task', async () => {
+        await mainPage.gotoLetCodeTables()
+        // not ready
+    });
+})
 
-test('Letcode assert Raj is checked Task', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoLetCodeTables()
-    await actionElements.checkRajCheckbox()
-    await expect(actionElements.rajCheckbox).toBeChecked()
-});
-
-test.skip('Letcode assert sorting is working correct Task', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const actionElements = new ActionElements(page)
-    await mainPage.gotoLetCodeTables()
-
-    // not ready
-});
